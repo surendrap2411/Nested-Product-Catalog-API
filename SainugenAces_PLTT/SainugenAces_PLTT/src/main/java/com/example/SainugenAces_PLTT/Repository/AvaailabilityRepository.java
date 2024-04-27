b530b2966c974759a2d33ec27917977599309092
Surendra Patil <156679725+surendrap2411@users.noreply.github.com>
2024-04-27 13:13:59 +0530

    Delete SainugenAces_PLTT/SainugenAces_PLTT directory

--- SainugenAces_PLTT/SainugenAces_PLTT/src/main/java/com/example/SainugenAces_PLTT/Repository/AvaailabilityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SainugenAces_PLTT.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.SainugenAces_PLTT.Entity.Availability;
-
-public interface AvaailabilityRepository extends JpaRepository<Availability,Long>{
-
-}
--- SainugenAces_PLTT/SainugenAces_PLTT/src/main/java/com/example/SainugenAces_PLTT/Repository/RatingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SainugenAces_PLTT.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.SainugenAces_PLTT.Entity.Rating;
-
-public interface RatingRepository extends JpaRepository<Rating,Integer>{
-
-}
